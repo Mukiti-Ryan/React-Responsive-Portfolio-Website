8c5c1ba914273356d0c98d83781058cf77d299e6
Mukiti-Ryan <87067381+Mukiti-Ryan@users.noreply.github.com>
2023-02-02 20:36:06 +0300

    Home Component Created, Edited and Completed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import "./Home.css";
+import Typewriter from "typewriter-effect";
+import MyCv from "./cv.pdf"
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="container-fluid home">
+            <div className="container home-content">
+                <h1>Hi. I'm a</h1>
+                <h3>
+                    <Typewriter options={{
+                        strings: [
+                            "Full Stack Software Developer",
+                            "MERN STACK",
+                            "Web Developer",
+                            "UI/UX Designer",
+                        ],
+                        autoStart: true,
+                        loop: true,
+                        delay: 5
+                    }}/>
+                </h3>
+
+                <div className="button-for-action">
+                    <div className="hire-me-button">Hire Me</div>
+                    <div className="get-resume-button"><a href={MyCv} download="Portfolio_Owner_cv">Get Resume</a></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
